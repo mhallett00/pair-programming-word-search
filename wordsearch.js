5e655a9876b6398f7ab6f77bea0482d30f56ab57
Michael Hallett <m.hallett@gmail.com>
2020-05-06 22:17:57 +0000

    changed function to allow for checking veritcally and returning false for empty arrays.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (letters.length) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    console.log(horizontalJoin, verticalJoin);
+    const orientation = [horizontalJoin, verticalJoin];
+    for (const each of orientation) {
+      for (const l of each) {
+        console.log(l);
+        if (l.includes(word)) {
+          return true;
+        }
+      }
     }
-}
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(search) {
+  const finalSearch = [];
+  for (let x = 0; x < search[0].length; x++) {
+    const newSearch = [];
+    for (let y = 0; y < search.length; y++) {
+      newSearch.push(search[y][x]);
+    }
+    finalSearch.push(newSearch);
+  }
+  return finalSearch;
+};
+
+
+
+
+module.exports = wordSearch;
